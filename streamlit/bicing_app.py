e727e251e96eab7cf67fcfeef1cf05d7a4c15ae3
QuimBassa <bassa.quim@gmail.com>
2024-06-12 15:22:38 +0200

    add notebook model_v1.ipynb

--- streamlit/bicing_app.py
+++ streamlit/bicing_app.py
@@ -1,3 +1,25 @@
 import streamlit as st
 
+# Title
 st.title("BICING APP")
+
+# Subtitle
+st.write("## Capstone Project")
+
+# Main content
+st.write(
+    """
+    Welcome to the BICING APP! This app is designed to display the results of our capstone project.
+    
+    Our Team form by:
+    - Pol
+    - Mario
+    - Quim Bassa
+    - Carles Onielfa
+    - Jacint
+
+    ### Project Overview
+    In this project, participants were challenged to develop a predictive model to forecast the availability of docked bikes at various bike-sharing stations in Barcelona. Bike sharing is a popular mode of transportation in many urban areas, and accurate predictions of bike availability can help riders plan their commutes and minimize wait times.
+    
+    Given a dataset with historical information of the stations, they were tasked with developing models that can accurately forecast the availability of docks at different times and locations.
+    """)
